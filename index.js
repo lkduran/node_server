4cda997a593ec31ac368c25286173e1c3c255dff
Leah Duran <lkduran@ucsc.edu>
2025-11-18 18:05:02 -0800

    frontend for heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const API_URL = 'https://tasks123.herokuapp.com';
+
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('http://localhost:5000/getAll')
+    fetch(API_URL + '/getAll')
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
 });
